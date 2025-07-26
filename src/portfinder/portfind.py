2a6afaba001b29479180288e566f78abab19bebf
Eilon H <eilonhafzadi7311@gmail.com>
2025-07-27 00:26:31 +0300

    Initial commit

--- src/portfinder/portfind.py
+++ src/portfinder/portfind.py
@@ -6,19 +6,64 @@ import socket
 from typing import Optional
 
 LOCAL_HOST_ADDRESS = "127.0.0.1"
+PING_REQUEST = "PING"
+
 LOWEST_PORT = 0
 HIGHEST_PORT = 65_535
 DEFAULT_TIMEOUT = 0.10
 
+
+class DatagramScanProtocol(asyncio.DatagramProtocol):
+    def __init__(self, ip, port, scan_finished: asyncio.Future):
+        self.ip = ip
+        self.port = port
+        self.scan_finished = scan_finished
+        self.transport = None
+
+    def datagram_received(self, data, addr):
+        print("Received:", data.decode())
+        if not self.scan_finished.done():
+            self.scan_finished.set_result(True)
+
+    def error_received(self, exc):
+        if not self.scan_finished.done():
+            self.scan_finished.set_exception(exc)
+
+    def connection_made(self, transport):
+        self.transport = transport
+        self.transport.sendto(PING_REQUEST.encode(), (self.ip, self.port))
+
+
 async def scan_port(ip, port, timeout):
     try:
-        reader, writer = await asyncio.wait_for(
-            asyncio.open_connection(ip, port),
-            timeout
+        return await scan_udp_port(ip, port, timeout)
+    except (asyncio.TimeoutError, OSError):
+        return None
+
+
+async def scan_tcp_port(ip, port, timeout):
+    _, writer = await asyncio.wait_for(
+        asyncio.open_connection(ip, port),
+        timeout
+    )
+
+    writer.close()
+    await writer.wait_closed()
+    return port
+
+
+async def scan_udp_port(ip, port, timeout):
+    try:
+        loop = asyncio.get_running_loop()
+        scan_finished = loop.create_future()
+
+        transport, _ = await loop.create_datagram_endpoint(
+            lambda: DatagramScanProtocol(ip, port, scan_finished),
+            remote_addr=(ip, port)
         )
 
-        writer.close()
-        await writer.wait_closed()
+        await asyncio.wait_for(scan_finished, timeout)
+        transport.close()
         return port
     except (asyncio.TimeoutError, OSError):
         return None
@@ -31,11 +76,13 @@ def is_ip_address(ip_str):
     except ValueError:
         return False
 
+
 def try_resolve_domain(domain):
     try:
         return socket.gethostbyname(domain)
     except socket.gaierror:
         raise typer.BadParameter(f"Invalid IP address '{domain}'.")
+
 
 def port_find(address: Optional[str] = typer.Argument(None, help="IP address to scan for open ports."),
               start: int = typer.Option(LOWEST_PORT, "--start", help="Starting port range.", min=LOWEST_PORT,
@@ -54,16 +101,18 @@ async def async_port_find(address, start, end, timeout):
     if address == "localhost":
         address = LOCAL_HOST_ADDRESS
 
-
     if not is_ip_address(address):
         address = try_resolve_domain(address)
 
     if start > end:
         raise typer.BadParameter("start port cannot be greater than end port.")
 
-    print("_" * 63)
-    print(f"Please wait, scanning {start} - {end} ports in remote host: {address}")
-    print("_" * 63)
+    message = f"Please wait, scanning {start} - {end} ports in remote host: {address}"
+    message_len = len(message)
+
+    print("_" * message_len)
+    print(message)
+    print("_" * message_len)
 
     semaphore = asyncio.Semaphore(100)
 
